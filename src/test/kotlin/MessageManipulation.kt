fde2f1ec407891d2c592bb1ea24eef2570fb3317
PandaBoy444 <hrsidsad@gmail.com>
2022-03-20 22:48:22 +0200

    Create dummy tests

---
+++ src/test/kotlin/MessageManipulation.kt
@@ -0,0 +1,32 @@
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import org.junit.jupiter.api.Assertions.*
+
+internal class MessageManipulation {
+
+
+
+    @Test
+    fun `create`(){
+        assert(false)
+    }
+    @Test
+    fun `retrieve single`() {
+        assert(false)
+    }
+    @Test
+    fun `retrieve all`() {
+        assert(false)
+    }
+    @Test
+    fun `update`() {
+        assert(false)
+    }
+    @Test
+    fun `delete`() {
+        assert(false)
+    }
+
+}
---
+++ src/test/kotlin/Routing.kt
@@ -0,0 +1,17 @@
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class Routing {
+    @Test
+    fun `root show hello world`() {
+        withTestApplication(Application::module) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                Assertions.assertEquals(HttpStatusCode.OK, response.status())
+                Assertions.assertEquals("Hello, world!", response.content)
+            }
+        }
+    }
+}
