af052f6e0e3dcac11311a671a8b15fa6fe54e94c
PandaBoy444 <hrsidsad@gmail.com>
2022-03-21 18:27:22 +0200

    Moved TestApplicationEngine extensions to their own file

---
+++ src/test/kotlin/TestApplicationEngineExtensions.kt
@@ -0,0 +1,22 @@
+import io.ktor.http.*
+import io.ktor.server.testing.*
+
+fun TestApplicationEngine.withCreateMessage(
+    messageText: String,
+    action: (TestApplicationResponse, Message) -> Unit
+) {
+    with(handleRequest(HttpMethod.Post, "/messages") {
+        addHeader(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())
+        setBody(listOf("text" to messageText).formUrlEncode())
+    }) {
+        action(response, Message(messageText))
+    }
+
+}
+
+fun TestApplicationEngine.withGetMessage(messageIndex: Int, action: (TestApplicationResponse, Message?) -> Unit) {
+    with(handleRequest(HttpMethod.Get, "/messages/$messageIndex")) {
+        action(response, MessagesDb.getMessage(messageIndex))
+    }
+
+}
