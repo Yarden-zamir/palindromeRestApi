94368c7362e5f957747294aa5b03ac440b82fe69
PandaBoy444 <hrsidsad@gmail.com>
2022-03-21 17:45:59 +0200

    Added calllogging

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,13 +2,19 @@ import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
     configureRouting()
     configureStatusPage()
     configureDatabase()
+    install(CallLogging)
 }
 
