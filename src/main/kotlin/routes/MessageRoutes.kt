9fe181ad904204a4f94fcf9809578834485901c3
PandaBoy444 <hrsidsad@gmail.com>
2022-03-22 07:46:39 +0200

    Separate Routing into extension functions to make it more readable

--- src/main/kotlin/routes/MessageRoutes.kt
+++ src/main/kotlin/routes/MessageRoutes.kt
@@ -12,40 +12,49 @@ import io.ktor.routing.*
 
 fun Route.messages() {
     route("/messages") {
-        post {
-            val parameters = call.receiveParameters()
-            val messageText = parameters["text"] ?: return@post call.respondText(
-                "No text parameter for message",
-                status = HttpStatusCode.BadRequest
-            )
-            val message = Message(messageText)
-            MessagesDb.addMessage(message)
-            call.respondText("Message created with id ${message.id}", status = HttpStatusCode.Created)
-        }
-        get {
-            if (MessagesDb.getMessages().isEmpty())
-                call.respondText(
-                    "No messages found",
-                    status = HttpStatusCode.NotFound
-                )
-            else call.respondText("Messages : " + MessagesDb.getMessages().toString(), status = HttpStatusCode.Found)
-
-        }
-        get("{id}") {
-            val idNumber = call.parameters["id"]!!.toIntOrNull() ?: return@get call.respondText(
-                //we know that parameter id will never be null as this route happens only when it gets past, therefore the '!!'
-                "Illegal id, use numerical ids",
-                status = HttpStatusCode.BadRequest
-            )
-            val message = MessagesDb.getMessages().getOrNull(idNumber)?.toString() ?: return@get call.respondText(
-                "No message with id $idNumber",
-                status = HttpStatusCode.NotFound
-            )
-            call.respondText(message, status = HttpStatusCode.Found)
-        }
+        createMessage()
+        getMessages()
+        getMessage()
         //maybe make it able to fetch by content too? ie get({content})
 
 
     }
+}
 
+private fun Route.createMessage() {
+    post {
+        val parameters = call.receiveParameters()
+        val messageText = parameters["text"] ?: return@post call.respondText(
+            "No text parameter for message", status = HttpStatusCode.BadRequest
+        )
+        val message = Message(messageText)
+        MessagesDb.addMessage(message)
+        call.respondText("Message created with id ${message.id}", status = HttpStatusCode.Created)
+    }
+}
+
+private fun Route.getMessages() {
+    get {
+        if (MessagesDb.getMessages().isEmpty()) call.respondText(
+            "No messages found", status = HttpStatusCode.NotFound
+        )
+        else call.respondText("Messages : " + MessagesDb.getMessages().toString(), status = HttpStatusCode.Found)
+
+    }
+}
+
+private fun Route.getMessage() {
+    get("{id}") {
+        println("GETTING MESSAGE")
+        val idNumber = call.parameters["id"]!!.toIntOrNull() ?: return@get call.respondText(
+            //we know that parameter id will never be null as this route happens only when it gets past, therefore the '!!'
+            "Illegal id, use numerical ids", status = HttpStatusCode.BadRequest
+        )
+        println("MESSAGE ID IS $idNumber")
+        val message = MessagesDb.getMessages().getOrNull(idNumber)?.toString() ?: return@get call.respondText(
+            "No message with id $idNumber", status = HttpStatusCode.NotFound
+        )
+        println("MESSAGE IS ${message.toString()}")
+        call.respondText(message, status = HttpStatusCode.Found)
+    }
 }
