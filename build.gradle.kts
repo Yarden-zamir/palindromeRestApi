624441dadea104423e47bf8bb5410d876cfdc9a8
PandaBoy444 <hrsidsad@gmail.com>
2022-03-23 09:19:46 +0200

    Started testing google cloud deployment
    So far it doesn't work for me. When the app is in the air it infinitely loads and doesn't present anything

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktorVersion = "1.6.8"
 val logbackVersion = "1.2.5"
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.google.cloud.tools.appengine") version "2.4.2"
+
     application
 }
+
 
 group = "me.charcoal"
 version = "1.0-SNAPSHOT"
@@ -37,10 +42,25 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "16"
-}
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions.jvmTarget = "16"
+//}
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("ApplicationKt")
+}
+
+appengine {
+    stage {
+
+//        artifact =
+        setArtifact("build/libs/${project.name}-${project.version}-all.jar")
+//        artifact =
+    }
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "messagesapi-344913"
+        stopPreviousVersion = true
+        promote = true
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
 
 rootProject.name = "palindromeRestApi"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
+                useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
